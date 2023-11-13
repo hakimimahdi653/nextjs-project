26f134ddbc6d8045088f61146edaa9bcf22b9347
hakimi <hakimimahdi653@gmail.com>
2023-11-13 09:00:58 +0330

    chapter 12

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -168,7 +168,7 @@ export async function fetchInvoiceById(id: string) {
       WHERE invoices.id = ${id};
     `;
 
-    const invoice = data.rows.map((invoice) => ({
+    const invoice = data.rows.map((invoice: InvoiceForm) => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
